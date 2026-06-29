addd304d11ee41d03e668498ee86c470c17b7d7c
val055 <146097320+val055@users.noreply.github.com>
2026-06-29 17:36:44 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print('Hello from repository!!!')
+from dotenv import load_dotenv
+import os
+
+
+
+def print_author():
+
+  load_dotenv(dotenv_path = '.env')
+  author = os.getenv('author')
+  print(f"Автор проекта: {author}")
+
+AUTHOR = print_author()
